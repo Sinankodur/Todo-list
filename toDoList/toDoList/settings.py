cadc34c0cb307d961a03bb73c27a946cfb166734
sinan kodur <sinankodur24@gmail.com>
2024-05-07 02:39:49 +0530

    ready for the deplyment

--- toDoList/toDoList/settings.py
+++ toDoList/toDoList/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7!301m5nwo&g@=gq8r-r0wq03rv63%xt(l3^ig4r#78@69^0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 LOGIN_URL = '/sign-in/'
 LOGOUT_REDIRECT_URL = '/'
